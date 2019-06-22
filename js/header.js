fdf3e1c2effd5b30e199878f99471a6d08b80da9
WangXiaoXin <wxx1_2_3@163.com>
2019-06-22 21:17:30 +0800

    first commit

---
+++ js/header.js
@@ -0,0 +1,35 @@
+document.write('\
+        <div class="wrapper">\
+        <div class="navmenuright">\
+            <ul>\
+                <div class="buttonleft">\
+                    <a href="javascript:history.go(-1);"><img src="images/frame/return.png"  alt=""/></a>\
+                </div>\
+                <div class="buttonright">\
+                        <a href="Navigation.html"><img src="images/frame/home.png"  alt=""/></a>\
+                </div>\
+                <div class="textleft">\
+                    返回\
+                </div>\
+                <div class="textright">\
+                    首页\
+                </div>\
+                <div class="dgjlogo">\
+                    <center><img src="images/frame/shou.png" alt="" /></center>\
+                </div>\
+            </ul>\
+        </div>\
+    </div>\
+    <style>\
+        .square {\
+            width: 100%;\
+            height: 25px;\
+            background-color:#B0E2FF;\
+            border-color:#1E90FF;\
+            border-style: solid;\
+            border-width:0px 1px 1px 1px;\
+            z-index:100;\
+        }\
+    </style>\
+        ');
+document.write('</br>')
